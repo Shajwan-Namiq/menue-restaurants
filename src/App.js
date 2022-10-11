2ca5e4e5c22cc7f659978d9cb020a3298bc9dd80
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-11 23:26:55 +0300

    get data from json file:

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "./App.css";
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
 
 function App() {
-  return( 
-  
-  <>
-  <div>hi</div>
-  
-  </>
-  
-  
-  )
+
+ const [menueItems, setMenueItems] = useState(items); 
+ const [categories ,setCategories] = useState([]);
+ 
+ return (
+   <>
+     <div className="flex justify-center p-5 text-2xl font-bold">
+       <p>
+         Ou<span className=" border-b-2 border-emerald-600">r Me</span>nue
+       </p>
+     </div>
+
+     <div>
+       <Categories />
+       <Menu items={menueItems}  />
+     </div>
+   </>
+ );
 }
 
 export default App;
---
+++ src/Menu.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+
+
+
+const Menu = ({ items }) => {
+
+  return (
+    <div>
+      {items.map((menuItem) => {
+    const { id, title, img, desc, price } = menuItem;
+
+  return (
+    <>
+      <div key={id}>
+        <section class="bg-white border-b-2 mx-4  ">
+          <div class="container px-6 py-5 mx-auto">
+            <div class="grid grid-cols-1 gap-8 mt-8 md:mt-10 md:grid-cols-2">
+              <div class="lg:flex">
+                <img
+                  class="object-cover w-full h-56 rounded-lg lg:w-64"
+                  src={img}
+                  alt={title}
+                />
+
+                <div class="flex flex-col py-6 lg:mx-6">
+                 
+                  <div className='flex justify-between'>
+                    <div class="text-xl font-semibold text-green-600   ">
+                      {title}
+                    </div>
+
+                    <div class="text-xl font-semibold text-green-600   ">
+                      ${price}
+                    </div>
+                  </div>
+
+                  <div class="mt-2 text-sm text-gray-700 ">{desc}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      ;
+    </>
+  );
+
+
+      })}
+    </div>
+  )
+}
+
+export default Menu
