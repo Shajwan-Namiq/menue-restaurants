8b361d7fff9c22eee16db10d74a5f6366301b256
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-12 11:01:34 +0300

    Adding transition to img and cart && organize style of cart showing two  cart at the same row

--- src/Menu.js
+++ src/Menu.js
@@ -6,50 +6,45 @@ import React from 'react'
 const Menu = ({ items }) => {
 
   return (
-    <div>
-      {items.map((menuItem) => {
-    const { id, title, img, desc, price } = menuItem;
+    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  ">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8   container px-6 py-14 mx-auto">
+        {items.map((menuItem) => {
+          const { id, title, img, desc, price } = menuItem;
 
-  return (
-    <>
-      <div key={id}>
-        <section class="bg-white border-b-2 mx-4  ">
-          <div class="container px-6 py-5 mx-auto">
-            <div class="grid grid-cols-1 gap-8 mt-8 md:mt-10 md:grid-cols-2">
-              <div class="lg:flex">
-                <img
-                  class="object-cover w-full h-56 rounded-lg lg:w-64"
-                  src={img}
-                  alt={title}
-                />
+          return (
+            <>
+              <div
+                key={id}
+                class="bg-[#dad7cd] transform transition duration-500 hover:scale-110 "
+              >
+                <div class="flex">
+                  <img
+                    class="  transition duration-500 hover:-scale-x-90  object-cover w-36 h-36  lg:w-56 lg:h-56 rounded-lg "
+                    src={img}
+                    alt={title}
+                  />
 
-                <div class="flex flex-col py-6 lg:mx-6">
-                 
-                  <div className='flex justify-between lg:border-b-2'>
-                    <div class="text-xl font-semibold text-green-600   ">
-                      {title}
+                  <div class="flex flex-col py-6 mx-2 lg:mx-6">
+                    <div className="flex justify-between lg:border-b-2">
+                      <div class="text-xl font-semibold text-green-600   ">
+                        {title}
+                      </div>
+
+                      <div class="text-xl font-semibold text-green-600   ">
+                        ${price}
+                      </div>
                     </div>
 
-                    <div class="text-xl font-semibold text-green-600   ">
-                      ${price}
-                    </div>
+                    <div class="mt-2 text-sm text-gray-700 ">{desc}</div>
                   </div>
-
-                  <div class="mt-2 text-sm text-gray-700 ">{desc}</div>
                 </div>
               </div>
-            </div>
-          </div>
-        </section>
+            </>
+          );
+        })}
       </div>
-      
-    </>
-  );
-
-
-      })}
     </div>
-  )
+  );
 }
 
 export default Menu
